5b1396388a48e2248fbc976fd07e22354b462a5b
shamsmahee <shamsulmahee@outlook.com>
2025-02-11 00:13:24 +0600

    await async

--- 12th.js
+++ 12th.js
@@ -1,3 +1,18 @@
-fetch("https://jsonplaceholder.typicode.com/users")
-    .then(res => res.json())
-    .then(data => console.log(data))
+// fetch("https://jsonplaceholder.typicode.com/user")
+//     .then(res => {
+//         if (!res.ok) {
+//             throw new Error(`HTTP error!status:${res.status}`)
+//         }
+//         return res.json();
+//     })
+
+
+async function fetchMoves() {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await res.json();
+    console.log(users);
+
+}
+
+fetchMoves()
+
